f828513ec674b4c54e73d939009b0fe9e90033d3
Sean Scott <stscott@stanford.edu>
2015-02-06 05:00:28 +0000

    Added idconfig to root directory

---
+++ idconfig.py
@@ -0,0 +1,6 @@
+# Must be added to ./Lowdown_Backend/Lowdown/Lowdown
+
+FACEBOOK_APP_ID = '578996548904671'
+FACEBOOK_APP_SECRET = '0d30c8516c93306cc60d555052f77adc'
+
+GIT_ROOT = '/home/ubuntu/CS194Project/'
