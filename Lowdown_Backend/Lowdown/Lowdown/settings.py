fa391606593b288120d58b7e4cd76b22f38751f5
Sean Scott <stscott@stanford.edu>
2015-02-06 06:18:39 +0000

    Added all the permissions to the token!

--- Lowdown_Backend/Lowdown/Lowdown/settings.py
+++ Lowdown_Backend/Lowdown/Lowdown/settings.py
@@ -108,10 +108,34 @@ SOCIAL_AUTH_FACEBOOK_SECRET = idconfig.FACEBOOK_APP_SECRET
 
 SOCIAL_AUTH_FACEBOOK_SCOPE = [
     'email',
-    'user_friends',
-    'user_birthday',
     'user_about_me',
+    'user_actions.books',
+    'user_actions.music',
+    'user_actions.news',
+    'user_actions.fitness',
+    'user_actions.video',
+    'user_activities',
+    'user_birthday',
+    'user_education_history',
+    'user_events',
+    'user_friends',
+    'user_games_activity',
+    'user_groups',
+    'user_hometown',
+    'user_interests',
+    'user_likes',
+    'user_location'
     'user_photos',
+    'user_relationship_details',
+    'user_relationships',
+    'user_religion_politics',
+    'user_status',
+    'user_tagged_places',
+    'user_videos',
+    'user_website',
+    'user_work_history',
+    # Extended
+    'read_friendlists',
 ]
 
 # Static files (CSS, JavaScript, Images)
