fe038c96cb3cc2c3cae253219e8cba6b938b580f
Sean Scott <stscott@stanford.edu>
2015-02-06 11:27:22 +0000

    Added views backend code for simple questions

--- Lowdown_Backend/Lowdown/Facebook_App/urls.py
+++ Lowdown_Backend/Lowdown/Facebook_App/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'Facebook_App.views.home', name='home'),
+    url(r'^quiz/(\d+)$', 'Facebook_App.views.quiz', name='quiz'),
    url('', include('social.apps.django_app.urls', namespace='social')),
    url('', include('django.contrib.auth.urls', namespace='auth')),
    url(r'^admin/', include(admin.site.urls)),
--- Lowdown_Backend/Lowdown/Facebook_App/views.py
+++ Lowdown_Backend/Lowdown/Facebook_App/views.py
@@ -7,31 +7,44 @@ import json
 
 logger = logging.getLogger(__name__)
 
+def get_data(request, target, fields):
+   social_user = request.user.social_auth.filter(
+      provider='facebook',
+   ).first()
+   if target is None:
+      target = social_user.uid
+   payload = {
+      'fields': fields,
+      'access_token': social_user.extra_data['access_token'],
+   }
+   url = 'https://graph.facebook.com/%s' % target
+   r = requests.get(url, params=payload)
+   logger.debug(r.url)
+   logger.debug(r.json())
+   return r.json()
+ 
 def home(request):
    # A map from name to photo url
-   friends = {}
+   friends = []
    try:
-      social_user = request.user.social_auth.filter(
-         provider='facebook',
-      ).first()
-      payload = {
-         'fields': 'id,name,picture',
-         'access_token': social_user.extra_data['access_token'],
-      }
-      url = 'https://graph.facebook.com/%s/friends' % social_user.uid
-      r = requests.get(url, params=payload)
-      logger.debug(r.url)
-      logger.debug(r.json())
-      for friend in r.json()['data']:
-         friends[friend['name']] = friend['picture']['data']['url']
+      r = get_data(request, None, 'friends{name,id,picture}')
+      friends = r['friends']['data']
    except AttributeError:
       logger.debug('Anonymous user')
    context = RequestContext(request,
                            {'request': request,
                             'friends': friends,
-                            'user': request.user})
+                           })
    return render_to_response('home.html',
                              context_instance=context)
+
+def quiz(request, friend_id):
+   statuses = get_data(request, friend_id, 'statuses')
+   context = RequestContext(request,
+                            {'request': request,
+                             'statuses': statuses,
+                            })
+   return render_to_response('quiz.html', context_instance=context)
 
 # Addison's Code
 
