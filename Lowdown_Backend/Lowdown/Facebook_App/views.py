1976f71a64dc85596e5e7e0107421510b7b3d1aa
Ubuntu <ubuntu@ip-172-31-18-209.us-west-1.compute.internal>
2015-02-08 23:34:39 +0000

    Got grading working!

--- Lowdown_Backend/Lowdown/Facebook_App/views.py
+++ Lowdown_Backend/Lowdown/Facebook_App/views.py
@@ -90,17 +90,43 @@ def quiz(request, friend_id):
     question2 = ImageCaptionQuestion(get_sized_photo(photo),
                                      caption,
                                      get_captions(photos, caption))
+    
     questions = [question1, question2]
+    
+    answers = []
+    for q in questions:
+      answers.append(q.random_index)
+
+    request.session['answers'] = answers
+
     context = RequestContext(request,
                              {'request': request,
                               'questions': questions,
                              })
+
     return render_to_response('quiz.html', context_instance=context)
 
 
 def quiz_grade(request):
+    correctAnswers = 0
+    incorrectAnswers = 0
+    answers = request.session.get('answers')
+
+    arr = []
+    for field in request.POST:
+      if "question" in str(field):
+        index = int(str(field[-1]))
+        if int(answers[index-1]) == int(request.POST[field]):
+          correctAnswers+=1
+        else:
+          incorrectAnswers+=1
+
+
     context = RequestContext(request,
                              {'results': request.POST,
+                              'answers': answers,
+                              'correct': correctAnswers,
+                              'incorrect': incorrectAnswers
                              })
     return render_to_response('quiz_score.html', context_instance=context)
 
