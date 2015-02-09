b26f666a894b475221d7074cdc787a8606e93034
April Yu <yu.april.s@gmail.com>
2015-02-09 12:25:19 +0000

    added a few new fake statuses

--- Lowdown_Backend/Lowdown/Facebook_App/fake_statuses.py
+++ Lowdown_Backend/Lowdown/Facebook_App/fake_statuses.py
@@ -2,4 +2,7 @@ fake_statuses = [
     'I hate finals.',
     'Lolll can\'t believe that just happened...',
     'Happy birthday to the best mother ever!',
+    'Here goes nothing!',
+    'This sucks...',
+    'Ugh...so stressed!',
 ]
