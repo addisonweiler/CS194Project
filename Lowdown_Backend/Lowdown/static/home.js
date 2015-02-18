15fe99c995c2d31adb9b14769bf2f075265d6da4
April Yu <yu.april.s@gmail.com>
2015-02-18 06:54:41 +0000

    added javascript generating quiz...

---
+++ Lowdown_Backend/Lowdown/static/home.js
@@ -0,0 +1,12 @@
+(function(){
+  	"use strict";
+
+  	$(document).ready(function(){
+	  	$(".quiz_btn").click(function(){
+	    	$.get($(this).attr("data-href"), function(data){
+	      		document.write(data);
+	    	});
+		$("#loader").removeClass("hidden")
+	  })
+  });
+}());
