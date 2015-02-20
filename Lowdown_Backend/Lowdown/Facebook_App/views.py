af403b6fd02e0d59a801aefca99ebf805aa4b2cf
Addison Weiler <alweiler@stanford.edu>
2015-02-20 10:18:46 +0000

    Fixed so app id is linked to idconfig file, also re-enabled html on quiz_score to display questions. Finally, beyonce is still there.

--- Lowdown_Backend/Lowdown/Facebook_App/views.py
+++ Lowdown_Backend/Lowdown/Facebook_App/views.py
@@ -6,7 +6,7 @@ from django.template.context import RequestContext
 
 from generate_quiz import generate_quiz
 from utils import get_data
-
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -17,47 +17,46 @@ def home(request):
     friends = r['friends']['data']
   except AttributeError:
     logger.debug('Anonymous user')
-    context = RequestContext(request,
-     {'request': request,
-     'friends': friends,
-     })
-    return render_to_response('home.html', context_instance=context)
+  
+  context = RequestContext(request,
+    {'request': request,
+    'friends': friends,
+  })
+  return render_to_response('home.html', context_instance=context)
 
-    def quiz(request, friend_id):
-      request.session['friend_id'] = friend_id
-      return generate_quiz(request, friend_id)
+def quiz(request, friend_id):
+  request.session['friend_id'] = friend_id
+  return generate_quiz(request, friend_id)
 
-      def quiz_grade(request):
-        correctAnswers = 0
-        answers = request.session.get('answers')
+def quiz_grade(request):
+  correctAnswers = 0
+  answers = request.session.get('answers')
 
-        questions = [pickle.loads(q) for q in request.session.get('questions')]
+  questions = [pickle.loads(q) for q in request.session.get('questions')]
+  arr = []
+  for field in request.POST:
+    if "question" in str(field):
+      index = int(str(field)[9:])
+      if int(answers[index]) == int(request.POST[field]):
+        correctAnswers+=1
+        questions[index].checked = int(request.POST[field])
 
-        arr = []
-        for field in request.POST:
-          if "question" in str(field):
-            index = int(str(field)[9:])
-            if int(answers[index]) == int(request.POST[field]):
-              correctAnswers+=1
-              questions[index].checked = int(request.POST[field])
-              total_questions = len(questions)
+  total_questions = len(questions)
 
-              share_message = "Hey there, I just correctly answered {0} out of {1} questions about you on Lowdown! Want to take a quiz?".format(correctAnswers, total_questions)
-              if correctAnswers == 0:
-               share_message = "Welp, I just correctly answered {0} out of {1} questions about you on Lowdown. Oops! Want to take a quiz?".format(correctAnswers, total_questions)
+  share_message = "Hey there, I just correctly answered {0} out of {1} questions about you on Lowdown! Want to take a quiz?".format(correctAnswers, total_questions)
+  if correctAnswers == 0:
+    share_message = "Welp, I just correctly answered {0} out of {1} questions about you on Lowdown. Oops! Want to take a quiz?".format(correctAnswers, total_questions)
 
-               friend_id = request.session['friend_id']
-               context = RequestContext(request,
-                 {'results': request.POST,
-                 'answers': answers,
-                 'correct': correctAnswers,
-                 'total_questions' : total_questions,
-                 'questions': questions,
-                 'share_message' : share_message,
-                              'full_url' :  request.build_absolute_uri('/'), #Get home page
-                              'friend_id' : friend_id,
-                              })
-
-
-
-               return render_to_response('quiz_score.html', context_instance=context)
+  friend_id = request.session['friend_id']
+  context = RequestContext(request,
+    {'results': request.POST,
+    'answers': answers,
+    'correct': correctAnswers,
+    'total_questions' : total_questions,
+    'questions': questions,
+    'share_message' : share_message,
+    'full_url' :  request.build_absolute_uri('/'), #Get home page
+    'friend_id' : friend_id,
+    'app_id' : settings.SOCIAL_AUTH_FACEBOOK_KEY
+  })
+  return render_to_response('quiz_score.html', context_instance=context)
--- Lowdown_Backend/Lowdown/static/quiz_score.js
+++ Lowdown_Backend/Lowdown/static/quiz_score.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
 	FB.init({
-		appId      : '590672707737055',
+		appId      : '{{app_id}}',
 		xfbml      : true,
 		version    : 'v2.1'
 	});
@@ -14,4 +14,19 @@ window.fbAsyncInit = function() {
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
+(function(){
+	"use strict";
+	$(document).ready(function(){
+		$(".share_btn").click(function(){
+			FB.ui({
+				method: 'feed',
+				name: "Lowdown Friend Quiz",
+				link: '{{full_url}}',
+				caption: 'Lowdown: The auto-generated Facebook Quiz!',
+				to: '{{friend_id}}',
+				description: '{{share_message}}',
+				picture: 'http://revelationnow.net/wp-content/gallery/beyonce_demon_dance_face/01_angry_beyonce.jpg',
+			}, function(response){});
+		})
+	});
+}());
