375d6c1617e0a6f54cd9334f47320346882a520c
Ubuntu <ubuntu@ip-172-31-12-91.us-west-1.compute.internal>
2015-02-23 02:53:41 +0000

    Removed unnecessary dit

--- Lowdown_Backend/Lowdown/lowdown_quiz_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'mysite.myapp.views.home', name="home"),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
--- Lowdown_Backend/Lowdown/lowdown_quiz_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django_facebook.decorators import canvas_only
-
-@canvas_only
-def home(request):
-    return render(request, 'home.html')
