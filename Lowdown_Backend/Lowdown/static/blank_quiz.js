097323de721f00e9618d7e5dfd118d18900beec2
April Yu <yu.april.s@gmail.com>
2015-03-02 21:13:17 +0000

    fixed javascript...I think

--- Lowdown_Backend/Lowdown/static/blank_quiz.js
+++ Lowdown_Backend/Lowdown/static/blank_quiz.js
@@ -2,9 +2,12 @@
   	"use strict";
 
   	$(document).ready(function(){
-	    $.get(window.location.href + "/content", function(data){
-	      	document.write(data);
-	    });
-	    window.setTimeout(function(){alert("Your quiz could not load.  Please try again");}, 10000)
+  		try {
+  			$.get(window.location.href + "/content", function(data){
+      			document.write(data);
+	    	});
+  		} catch(e) {
+  			alert("Your quiz failed to load.  Please try again.");
+  		}
   	});
 }());
