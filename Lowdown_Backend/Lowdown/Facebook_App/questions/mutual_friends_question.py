63ee0357c92c763c5b1f217735fab5c7fb1b079c
Ubuntu <alweiler@stanford.edu>
2015-03-18 07:43:51 +0000

    Mutual friends turned on

--- Lowdown_Backend/Lowdown/Facebook_App/questions/mutual_friends_question.py
+++ Lowdown_Backend/Lowdown/Facebook_App/questions/mutual_friends_question.py
@@ -14,10 +14,10 @@ class MutualFriendsQuestion(MultipleChoiceQuestion):
 
     @classmethod
     def gen(cls, self_data, friend_data):
-        mutual_friends = [(context['id'], context['name']) for context in
+        mutual_friends = [context['name'] for context in
                           get_paged_data(friend_data['context'],
                                          'mutual_friends')]
-        non_mutual_friends = [(friends['id'], friends['name']) for friends in
+        non_mutual_friends = [friends['name'] for friends in
                               get_paged_data(friend_data, 'friends')]
         logger.debug(mutual_friends)
         return cls(mutual_friends, non_mutual_friends)
